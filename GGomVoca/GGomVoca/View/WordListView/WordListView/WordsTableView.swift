88b70cedfc1e9dabf467528d3f3798a03540b20e
ehvkddl <ehvkddl@naver.com>
2023-01-20 15:51:51 +0900

    style(TempVocabulary): 임시 데이터 구조체, 열거형 제거 #16

--- GGomVoca/GGomVoca/View/WordListView/WordListView/WordsTableView.swift
+++ GGomVoca/GGomVoca/View/WordListView/WordListView/WordsTableView.swift
@@ -7,17 +7,6 @@
 
 import SwiftUI
 
-enum TempNationality: String, CaseIterable {
-    case KO = "한국어" // 한국어
-    case EN = "영어" // 영어
-    case JP = "일본어" // 일본어
-    case CH = "중국어" // 중국어
-    case FR = "프랑스어" // 프랑스어
-    case DE = "독일어" // 독일어
-    case ES = "스페인어" // 스페인어
-    case IT = "이탈리아어" // 이탈리아어
-}
-
 struct WordsTableView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
