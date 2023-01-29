b93f223dd1f291eca749311759684f569bdd41ac
steady-on <whitebsm93@naver.com>
2023-01-30 04:29:42 +0900

    Refactor #36
    - 한국어, 영어, 프랑스어, 일본어 각각에 대응하는 WordList 생성

---
+++ GGomVoca/GGomVoca/View/WordListView/JPWordList/JPAddNewWordView.swift
@@ -0,0 +1,119 @@
+//
+//  JPAddNewWordView.swift
+//  GGomVoca
+//
+//  Created by Roen White on 2022/12/20.
+//
+
+import SwiftUI
+
+struct JPAddNewWordView: View {
+    // MARK: Super View Properties
+    var viewModel : JPWordListViewModel
+    @Binding var addNewWord: Bool
+    
+    // MARK: View Properties
+    @State private var isContinue: Bool = false
+    /// - 입력값 관련
+    @State private var inputWord: String = ""
+    @State private var inputOption: String = ""
+    @State private var inputMeaning: String = ""
+    
+    // 입력값 공백 제거
+    private var word: String {
+        inputWord.trimmingCharacters(in: .whitespaces)
+    }
+    private var option: String {
+        inputOption.trimmingCharacters(in: .whitespaces)
+    }
+    private var meaning: String {
+        inputMeaning.trimmingCharacters(in: .whitespaces)
+    }
+    
+    // 입력값이 공백일 때 경고메세지 출력 조건
+    @State private var isWordEmpty: Bool = false
+    @State private var isMeaningEmpty: Bool = false
+    
+    // 추가 후 TextFeildFocus 이동
+    @FocusState private var wordFocused: Bool
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Toggle("입력창 고정하기", isOn: $isContinue)
+                    .toggleStyle(.switch)
+                
+                Section {
+                    TextField("단어를 입력하세요.", text: $inputWord, axis: .vertical)
+                        .textInputAutocapitalization(.never)
+                        .focused($wordFocused)
+                } header: {
+                    HStack {
+                        Text("단어")
+                        if isWordEmpty {
+                            Text("\(Image(systemName: "exclamationmark.circle")) 필수 입력 항목입니다.")
+                        }
+                    }
+                }
+                
+                Section(header: Text("발음")) {
+                    TextField("발음을 입력하세요.", text: $inputOption, axis: .vertical)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    TextField("뜻을 입력하세요.", text: $inputMeaning, axis: .vertical)
+                        .textInputAutocapitalization(.never)
+                } header: {
+                    HStack {
+                        Text("뜻")
+                        if isMeaningEmpty {
+                            Text("\(Image(systemName: "exclamationmark.circle")) 필수 입력 항목입니다.")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("새 단어 추가")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("취소", role: .cancel) {
+                        addNewWord = false
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("추가") {
+                        word.isEmpty ? (isWordEmpty = true) : (isWordEmpty = false)
+                        meaning.isEmpty ? (isMeaningEmpty = true) : (isMeaningEmpty = false)
+                        
+                        if !isWordEmpty && !isWordEmpty {
+                            /// - 단어 추가
+                            viewModel.addNewWord(word: word, meaning: meaning, option: option)
+                            
+                            /// - 단어 추가 후 textField 비우기
+                            inputWord = ""
+                            inputMeaning = ""
+                            inputOption = ""
+
+                            /// - isContinue 상태에 따라 sheet를 닫지 않고 유지함
+                            if !isContinue {
+                                addNewWord = false
+                                /// - 단어를 입력하는 TextField로 Focus 이동
+                                wordFocused = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+//struct JPAddNewWordView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NavigationStack {
+//            JPAddNewWordView(vocabulary: , isShowingAddWordView: .constant(true))
+//        }
+//    }
+//}
---
+++ GGomVoca/GGomVoca/View/WordListView/JPWordList/JPWordsTableView.swift
@@ -0,0 +1,103 @@
+//
+//  JPWordsTableView.swift
+//  GGomVoca
+//
+//  Created by do hee kim on 2023/01/18.
+//
+
+import SwiftUI
+
+struct JPWordsTableView: View {
+    // MARK: SuperView Properties
+    @ObservedObject var viewModel: JPWordListViewModel
+    var selectedSegment: ProfileSection
+    @Binding var unmaskedWords: [Word.ID]
+    
+    // MARK: View Properties
+    /// - 단어 수정 관련 State
+    @State var editWord: Bool = false
+    @State var editingWord: Word = Word()
+    
+    /// - 단어 리스트 편집 관련 State
+    @Binding var isSelectionMode: Bool
+    @Binding var multiSelection: Set<Word>
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+                Section {
+                    ForEach(viewModel.words) { word in
+                        JPWordCell(selectedSegment: selectedSegment, unmaskedWords: $unmaskedWords,
+                                 isSelectionMode: $isSelectionMode, multiSelection: $multiSelection,
+                                 nationality: viewModel.nationality, word: word)
+                            .addSwipeButtonActions(leadingButtons: [],
+                                              trailingButton:  [.delete], onClick: { button in
+                                switch button {
+                                case .delete:
+                                    viewModel.deleteWord(word: word)
+                                default:
+                                    print("default")
+                                }
+                            })
+                            .contextMenu {
+                                if selectedSegment == .normal {
+                                    Button {
+                                        editingWord = word
+                                        editWord.toggle()
+                                    } label: {
+                                        Label("수정하기", systemImage: "gearshape.fill")
+                                    }
+                                    
+                                    Button {
+                                        // Voice Over
+                                    } label: {
+                                        Label("발음 듣기", systemImage: "mic.fill")
+                                    }
+                                }
+                            }
+                        
+                        Divider()
+                    }
+                } header: {
+                    VStack(spacing: 0) {
+                        HStack {
+                            if isSelectionMode {
+                                /// - 편집모드에서 header도 체크용 원만큼 오른쪽으로 밀리도록 하기 위해 circle image를 배경색과 같은색으로 띄움
+                                Image(systemName: "circle")
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                                    .foregroundColor(Color("offwhite"))
+                                    .padding(.leading, 20)
+                            }
+                            
+                            Text("단어")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                            Text("발음")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                            Text("뜻")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                        .frame(height: 40)
+                        .background { Color("offwhite") }
+                        
+                        Rectangle()
+                            .foregroundColor(Color("toolbardivider"))
+                            .frame(height: 1)
+                    }
+                } // Section
+            } // LazyVStack
+            .background { Color("offwhite") }
+        } // ScrollView
+        // MARK: 단어 편집 시트
+        .sheet(isPresented: $editWord) {
+            JPEditWordView(viewModel: viewModel, editWord: $editWord, editingWord: $editingWord)
+                .presentationDetents([.medium])
+        }
+    }
+}
+
+//struct JPWordsTableView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        JPWordsTableView(selectedSegment: .constant(.normal), selectedWord: .constant([]), filteredWords: .constant(FRWords), isSelectionMode: .constant(false), multiSelection: .constant(Set<String>()), nationality: .FR)
+//    }
+//}
