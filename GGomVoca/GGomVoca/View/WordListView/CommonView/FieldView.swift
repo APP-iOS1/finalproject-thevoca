f2efb91dde515f7aa27e532a8f74e3bbca86625c
gxdxt <yxxsxxny@gmail.com>
2023-02-03 04:44:46 +0900

    feat(FieldView): FieldView 생성 #78

---
+++ GGomVoca/GGomVoca/View/WordListView/CommonView/FieldView.swift
@@ -0,0 +1,25 @@
+//
+//  FieldView.swift
+//  GGomVoca
+//
+//  Created by tae on 2023/02/03.
+//
+
+import SwiftUI
+
+struct FieldView: View {
+  @Binding var value: String
+  let onDelete: () -> Void
+
+  var body: some View {
+    HStack {
+      TextField("뜻을 입력하세요.", text: $value)
+      Button(action: {
+        print("clicked?")
+        onDelete()
+      }, label: {
+        Image(systemName: "multiply")
+      })
+    }
+  }
+}
