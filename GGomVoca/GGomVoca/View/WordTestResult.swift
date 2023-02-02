8fb7531780e48dce17a584e08db607475bbcb958
ehvkddl <ehvkddl@naver.com>
2023-02-02 13:23:04 +0900

    feat: 시험 보기와 결과지 View #67

--- GGomVoca/GGomVoca/View/WordTestResult.swift
+++ GGomVoca/GGomVoca/View/WordTestResult.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct WordTestResult: View {
+    @Binding var isTestMode: Bool
+    
+    @ObservedObject var paperViewModel: TestViewModel
+    let testMode: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                isTestMode = false
+            } label: {
+                HStack(spacing: 5) {
+                    Image(systemName: "chevron.backward")
+                        .font(.title2)
+                        .fontWeight(.medium)
+                    Text("단어장으로 돌아가기")
+                }
+                .padding(.leading, 8)
+                .padding(.vertical, 5)
+            }
+            .horizontalAlignSetting(.leading)
+            
+            Spacer()
+            
+            List(paperViewModel.testPaper) { paper in
+                HStack {
+                    switch testMode {
+                    case "word":
+                        Text(paper.isCorrect ? Image(systemName: "circle") : Image(systemName: "xmark"))
+                            .foregroundColor(.red)
+                        Text(paper.answer ?? "")
+                            .horizontalAlignSetting(.center)
+                        Text(paper.meaning)
+                            .horizontalAlignSetting(.center)
+                    case "meaning":
+                        Text(paper.isCorrect ? Image(systemName: "circle") : Image(systemName: "xmark"))
+                            .foregroundColor(.red)
+                        Text(paper.word)
+                            .horizontalAlignSetting(.center)
+                        Text(paper.answer ?? "")
+                            .horizontalAlignSetting(.center)
+                    default:
+                        Text("Empty")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            
+            Spacer()
+            
+        }
+        .navigationBarBackButtonHidden(true)
+        
     }
 }
 
-struct WordTestResult_Previews: PreviewProvider {
-    static var previews: some View {
-        WordTestResult()
-    }
-}
+//struct WordTestResult_Previews: PreviewProvider {
+//    static var previews: some View {
+//        WordTestResult()
+//    }
+//}
--- GGomVoca/GGomVoca/View/iPhoneWordTestView.swift
+++ GGomVoca/GGomVoca/View/iPhoneWordTestView.swift
@@ -7,14 +7,118 @@
 
 import SwiftUI
 
+enum Field: Hashable {
+    case answer
+}
+
 struct iPhoneWordTestView: View {
+    // 시험지 fullscreen 닫기 위한 Property
+    @Binding var isTestMode: Bool
+    
+    // MARK: Data Properties
+    var vocabularyID: Vocabulary.ID
+    @ObservedObject var viewModel: JPWordListViewModel
+    @StateObject var paperViewModel: TestViewModel = TestViewModel()
+    
+    // MARK: Test Mode에 관한 Properties
+    let testMode: String
+    let isMemorized: Bool
+    
+    // MARK: TextField에 관한 Properties
+    @FocusState private var focusedField: Field?
+    @State var answer: String = ""
+    // testMode에 따른 placeholder
+    var textFieldPlaceHolder: String {
+        switch testMode {
+        case "word":
+            return "단어를 입력해주세요"
+        case "meaning":
+            return "뜻을 입력해주세요"
+        default:
+            return ""
+        }
+    }
+    
+    var timer: String {
+        paperViewModel.timeRemaining == -1 ? "⏰ Time Over" : paperViewModel.convertSecondsToTime()
+    }
+    var isDisabled: Bool {
+        paperViewModel.timeRemaining == -1 ? true : false
+    }
+    
+    // 시험 종료 후 결과지로 이동하기 위한 Property
+    @State var isFinished: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Spacer()
+            
+            if !paperViewModel.testPaper.isEmpty {
+                Text(paperViewModel.showQuestion(testMode: testMode))
+                    .font(.largeTitle)
+            }
+            
+            Spacer()
+            
+            TextField("\(textFieldPlaceHolder)", text: $answer)
+                .multilineTextAlignment(.center)
+                .textFieldStyle(.roundedBorder)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                .focused($focusedField, equals: .answer)
+                .onSubmit {
+                    paperViewModel.saveAnswer(answer: answer)
+                    answer.removeAll()
+                    paperViewModel.restartTimer()
+                    focusedField = .answer
+                }
+                .disabled(isDisabled)
+        }
+        .onAppear {
+            paperViewModel.createPaper(words: viewModel.words, isMemorized: isMemorized)
+            paperViewModel.startTimer()
+            focusedField = .answer
+        }
+        .navigationTitle("\(timer)")
+        .toolbar {
+            // 마지막 문제일 때는 제출 버튼
+            if paperViewModel.showSubmitButton() {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        paperViewModel.saveAnswer(answer: answer)
+                        // 타이머 종료
+                        paperViewModel.cancelTimer()
+                        // 문제지 채점
+                        paperViewModel.gradeTestPaper(testMode: testMode)
+                        isFinished = true
+                    } label: {
+                        Text("제출")
+                    }
+                    .navigationDestination(isPresented: $isFinished) {
+                        WordTestResult(isTestMode: $isTestMode, paperViewModel: paperViewModel, testMode: testMode)
+                    }
+                }
+                // 마지막 문제가 아닐 때는 PASS 버튼
+            } else {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        paperViewModel.saveAnswer(answer: "")
+                        answer.removeAll()
+                        paperViewModel.restartTimer()
+                        focusedField = .answer
+                    } label: {
+                        Text("PASS")
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
-struct iPhoneWordTestView_Previews: PreviewProvider {
-    static var previews: some View {
-        iPhoneWordTestView()
-    }
-}
+//struct iPhoneWordTestView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        iPhoneWordTestView(vocabularyID: UUID(uuidString: "64883267-186C-4053-A38E-940E6F6E7B41"), viewModel: JPWordListViewModel(), testMode: "meaning", isMemorized: true)
+//    }
+//}
