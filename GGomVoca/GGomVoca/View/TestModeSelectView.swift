d6ab5e10f909357350b68ee4a7ee45e2d2556347
ehvkddl <ehvkddl@naver.com>
2023-02-02 13:21:04 +0900

    feat(TestModeSelectView): 임시 시험 모드 선택 View #67

--- GGomVoca/GGomVoca/View/TestModeSelectView.swift
+++ GGomVoca/GGomVoca/View/TestModeSelectView.swift
@@ -8,13 +8,84 @@
 import SwiftUI
 
 struct TestModeSelectView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Binding var isTestMode: Bool
+    
+    // MARK: Data Properties
+    var vocabularyID: Vocabulary.ID
+    @ObservedObject var viewModel: JPWordListViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "xmark")
+            }
+            .horizontalAlignSetting(.trailing)
+            .padding()
+            
+            Spacer()
+            
+            Group {
+                NavigationLink {
+                    iPhoneWordTestView(
+                        isTestMode: $isTestMode,
+                        vocabularyID: vocabularyID,
+                        viewModel: viewModel,
+                        testMode: "word",
+                        isMemorized: true
+                    )
+                } label: {
+                    Text("전체 단어 시험")
+                }
+
+                
+                NavigationLink {
+                    iPhoneWordTestView(
+                        isTestMode: $isTestMode,
+                        vocabularyID: vocabularyID,
+                        viewModel: viewModel,
+                        testMode: "meaning",
+                        isMemorized: true
+                    )
+                } label: {
+                    Text("전체 뜻 시험")
+                }
+                
+                NavigationLink {
+                    iPhoneWordTestView(
+                        isTestMode: $isTestMode,
+                        vocabularyID: vocabularyID,
+                        viewModel: viewModel,
+                        testMode: "word",
+                        isMemorized: false
+                    )
+                } label: {
+                    Text("못외운 단어 시험")
+                }
+                
+                NavigationLink {
+                    iPhoneWordTestView(
+                        isTestMode: $isTestMode,
+                        vocabularyID: vocabularyID,
+                        viewModel: viewModel,
+                        testMode: "meaning",
+                        isMemorized: false
+                    )
+                } label: {
+                    Text("못외운 뜻 시험")
+                }
+            }
+            
+            Spacer()
+
+        }
     }
 }
 
-struct TestModeSelectView_Previews: PreviewProvider {
-    static var previews: some View {
-        TestModeSelectView()
-    }
-}
+//struct TestModeSelectView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TestModeSelectView(vocabularyID: UUID(uuidString: "64883267-186C-4053-A38E-940E6F6E7B41"), viewModel: JPWordListViewModel())
+//    }
+//}
