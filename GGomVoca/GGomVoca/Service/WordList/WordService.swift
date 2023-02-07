bbdda0ee2b9271b72e5e9d5abdd1848327c3a174
eigen98 <terrantrace@gmail.com>
2023-02-07 10:11:26 +0900

    Feat(WordListService): WordService Protocol 작성 close : #133

--- GGomVoca/GGomVoca/Service/WordList/WordService.swift
+++ GGomVoca/GGomVoca/Service/WordList/WordService.swift
@@ -11,15 +11,26 @@ protocol WordService{
     
     //MARK: 데이터를 디스크에 저장
     func saveContext()
-   //MARK: 단어장 리스트 불러오기
+    // MARK: 일치하는 id의 단어장 불러오기
     func fetchWordList() -> AnyPublisher<[Word], FirstPartyRepoError>
-    //MARK: 단어장 추가하기
-    func postWordData(vocaName : String, nationality: String) -> AnyPublisher<Vocabulary, FirstPartyRepoError>
-    //MARK: 단어장 고정 상태 업데이트
+    // MARK: 단어 추가하기
+    func postWordData(vocaName : String, nationality: String) -> AnyPublisher<Word, FirstPartyRepoError>
+    
+    // MARK: 단어 수정하기
     //TODO: Publisher 반환타입 수정
-    func updatePinnedVoca(id: UUID) -> AnyPublisher<String, FirstPartyRepoError>
+    func updateWord(id: UUID) -> AnyPublisher<String, FirstPartyRepoError>
 
-    //MARK: 단어장 삭제
+    // MARK: 단어 삭제하기
     //TODO: Publisher 반환타입 수정
-    func deletedVocaData(id: UUID) -> AnyPublisher<String, FirstPartyRepoError>
+    func deleteWord(id: UUID) -> AnyPublisher<String, FirstPartyRepoError>
+    
+    
+    
+    
+
+   
+   
+   
+   
+   
 }
