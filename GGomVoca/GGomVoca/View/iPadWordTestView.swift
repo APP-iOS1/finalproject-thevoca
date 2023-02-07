7867c4ccbdd0be77f2ea85fa23ac74f533c94f7c
ehvkddl <ehvkddl@naver.com>
2023-02-08 00:23:54 +0900

    refact(iPadWordTest): 사용되지 않는 코드 정리, View 정리 #136
    - test용 struct와 array 제거
    - 중복된 NavigationStack 제거
    - Header 분기처리 수정

--- GGomVoca/GGomVoca/View/iPadWordTestView.swift
+++ GGomVoca/GGomVoca/View/iPadWordTestView.swift
@@ -11,21 +11,6 @@ import SwiftUI
 // [] "word", "meaning"에 따라 switch로 분기
 // [] isMemorized가 false면 못외운 단어만 시험 보기
 
-struct TestWord: Identifiable {
-    let id = UUID()
-    var word: String
-    var meaning: String
-}
-
-private var testWords: [TestWord] = [
-    TestWord(word: "昇進", meaning: "승진"),
-    TestWord(word: "嫌がる", meaning: "싫어하다"),
-    TestWord(word: "宣伝", meaning: "선전"),
-    TestWord(word: "こっそり", meaning: "몰래, 살짝, 가만히"),
-    TestWord(word: "積む", meaning: "쌓다"),
-    TestWord(word: "素敵", meaning: "멋있다, 훌륭하다")
-]
-
 struct iPadWordTestView: View {
     // 시험지 fullscreen 닫기 위한 Property
     @Binding var isTestMode: Bool
@@ -50,50 +35,49 @@ struct iPadWordTestView: View {
     @State var isFinished: Bool = false
     
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                LazyVStack(spacing: 0, pinnedViews: .sectionHeaders) {
-                    Section {
-                        ForEach(vm.testPaper.indices, id: \.self) { index in
-                            VStack(spacing: 0) {
-                                HStack(alignment: .center) {
-                                    Text(testType == "meaning" ? vm.testPaper[index].word : vm.testPaper[index].meaning.joined(separator: ", "))
-                                        .multilineTextAlignment(.center)
-                                        .frame(width: 200, height: 80)
-                                    Divider()
-                                    TextField("", text: $answers[index], axis: .vertical)
-                                        .textInputAutocapitalization(.never)
-                                        .disableAutocorrection(true)
-                                        .padding(.vertical)
-                                }
+        ScrollView {
+            LazyVStack(spacing: 0, pinnedViews: .sectionHeaders) {
+                Section {
+                    ForEach(vm.testPaper.indices, id: \.self) { index in
+                        VStack(spacing: 0) {
+                            HStack(alignment: .center) {
+                                Text(testType == "meaning" ? vm.testPaper[index].word : vm.testPaper[index].meaning.joined(separator: ", "))
+                                    .multilineTextAlignment(.center)
+                                    .frame(width: 200, height: 80)
                                 Divider()
+                                TextField("", text: $answers[index], axis: .vertical)
+                                    .textInputAutocapitalization(.never)
+                                    .disableAutocorrection(true)
+                                    .padding(.vertical)
                             }
-                        }
-                    } header: {
-                        VStack(spacing:10) {
                             Divider()
-                            
-                            HStack {
-                                if testType == "word" {
-                                    Text("입력 언어를 해당 하는 언어로 변경하고,")
-                                }
-                                
-                                Text("펜슬로 빈 칸에 답을 적어주세요.")
-                            }
-                            
+                        }
+                    }
+                } header: {
+                    VStack(spacing:10) {
+                        Divider()
+                        
+                        HStack {
                             if testType == "word" {
-                                Text("뜻이 여러 개인 경우 ,(쉼표)로 구분해 주세요.")
+                                Text("입력 언어를 해당 하는 언어로 변경하고,")
                             }
                             
-                            Divider()
+                            Text("펜슬로 빈 칸에 답을 적어주세요.")
+                            
                         }
-                        .background { Color("offwhite") }
-                    } footer: {
-                        VStack(spacing:0) {
-                            Divider()
-                            Text("우상단의 제출버튼을 눌러주세요.").padding(10)
-                            Divider()
+                        
+                        if testType == "meaning" {
+                            Text("뜻이 여러 개인 경우 ,(쉼표)로 구분해 주세요.")
                         }
+                        
+                        Divider()
+                    }
+                    .background { Color("offwhite") }
+                } footer: {
+                    VStack(spacing:0) {
+                        Divider()
+                        Text("우상단의 제출버튼을 눌러주세요.").padding(10)
+                        Divider()
                     }
                 }
             }
