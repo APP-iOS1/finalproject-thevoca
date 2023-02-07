86d5e3aa72cc0618d8b6c1077d5525237764b2bc
gxdxt <yxxsxxny@gmail.com>
2023-02-07 19:18:39 +0900

    feat(Segment): enum rawValue changed #128

---
+++ GGomVoca/GGomVoca/View/WordListView/CommonView/HamburgerButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  HamburgerButtonView.swift
+//  GGomVoca
+//
+//  Created by tae on 2023/02/07.
+//
+
+import SwiftUI
+
+struct HamburgerButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HamburgerButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        HamburgerButtonView()
+    }
+}
