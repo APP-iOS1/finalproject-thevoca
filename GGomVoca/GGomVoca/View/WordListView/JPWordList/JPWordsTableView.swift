60f58faf6fbb489ba2e08718d046dac9b54abbdc
ehvkddl <ehvkddl@naver.com>
2023-02-08 16:36:34 +0900

    feat: 편집 모드에서 cell과 header가 정렬이 맞도록 수정 #153

--- GGomVoca/GGomVoca/View/WordListView/JPWordList/JPWordsTableView.swift
+++ GGomVoca/GGomVoca/View/WordListView/JPWordList/JPWordsTableView.swift
@@ -25,6 +25,10 @@ struct JPWordsTableView: View {
     var body: some View {
         // MARK: - Header
         HStack {
+            if isSelectionMode {
+                Image(systemName: "circle")
+                    .foregroundColor(.clear)
+            }
             Text("단어")
                 .headerText()
             Text("발음")
@@ -38,50 +42,51 @@ struct JPWordsTableView: View {
                 .fill(Color("fourseason"))
                 .frame(height: 30)
         }
+        .animation(Animation.spring(), value: isSelectionMode)
         
         // MARK: - Content
         List($viewModel.words, id: \.self, selection: $multiSelection) { $word in
-                HStack {
-                    // 단어
-                    Text(word.word ?? "")
-                        .horizontalAlignSetting(.center)
-                        .multilineTextAlignment(.center)
-                        .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
-                    // 발음
-                    Text(word.option ?? "")
-                        .horizontalAlignSetting(.center)
-                        .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
-                    // 뜻
-                    Text(word.meaning!.joined(separator: ", "))
-                        .horizontalAlignSetting(.center)
-                        .opacity((selectedSegment == .meaningTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+            HStack {
+                // 단어
+                Text(word.word ?? "")
+                    .horizontalAlignSetting(.center)
+                    .multilineTextAlignment(.center)
+                    .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+                // 발음
+                Text(word.option ?? "")
+                    .horizontalAlignSetting(.center)
+                    .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+                // 뜻
+                Text(word.meaning!.joined(separator: ", "))
+                    .horizontalAlignSetting(.center)
+                    .opacity((selectedSegment == .meaningTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+            }
+            .frame(minHeight: 40)
+            .alignmentGuide(.listRowSeparatorLeading) { d in
+                d[.leading]
+            }
+            .swipeActions(allowsFullSwipe: false) {
+                Button(role: .destructive){
+                    viewModel.deleteWord(word: word)
+                }label: {
+                    Label("Delete", systemImage: "trash.fill")
                 }
-                .frame(minHeight: 40)
-                .alignmentGuide(.listRowSeparatorLeading) { d in
-                    d[.leading]
-                }
-                .swipeActions(allowsFullSwipe: false) {
-                    Button(role: .destructive){
-                        viewModel.deleteWord(word: word)
-                    }label: {
-                        Label("Delete", systemImage: "trash.fill")
+            }
+            .contextMenu {
+                if selectedSegment == .normal {
+                    Button {
+                        editingWord = word
+                        editWord.toggle()
+                    } label: {
+                        Label("수정하기", systemImage: "gearshape.fill")
+                    }
+                    Button {
+                        SpeechSynthesizer.shared.speakWordAndMeaning(word, to: "ja-JP", .single)
+                    } label: {
+                        Label("발음 듣기", systemImage: "mic.fill")
                     }
                 }
-                .contextMenu {
-                    if selectedSegment == .normal {
-                        Button {
-                            editingWord = word
-                            editWord.toggle()
-                        } label: {
-                            Label("수정하기", systemImage: "gearshape.fill")
-                        }
-                        Button {
-                            SpeechSynthesizer.shared.speakWordAndMeaning(word, to: "ja-JP", .single)
-                        } label: {
-                            Label("발음 듣기", systemImage: "mic.fill")
-                        }
-                    }
-                }
+            }
         } // List
         .listStyle(.plain)
         .padding(.top, -10)
