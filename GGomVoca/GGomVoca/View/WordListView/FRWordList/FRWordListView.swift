7cb6472fec879afcfde9286d338e31b133788b72
gxdxt <yxxsxxny@gmail.com>
2023-02-10 03:40:22 +0900

    feat(WordListView): NavigationLink 대체 적용 - NavigationStack 추가 - navigationDestination 추가 - 모든 언어 단어장에 적용
    Features #177

--- GGomVoca/GGomVoca/View/WordListView/FRWordList/FRWordListView.swift
+++ GGomVoca/GGomVoca/View/WordListView/FRWordList/FRWordListView.swift
@@ -55,179 +55,187 @@ struct FRWordListView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            
-            if viewModel.words.isEmpty {
-                VStack(spacing: 10) {
-                    EmptyWordListView(lang: viewModel.nationality)
-                }
-                .foregroundColor(.gray)
-                .verticalAlignSetting(.center)
-            } else {
-                FRWordsTableView(viewModel: viewModel, selectedSegment: selectedSegment, unmaskedWords: $unmaskedWords, isSelectionMode: $isSelectionMode, multiSelection: $multiSelection)
-                    .padding(.top, 15)
-            }
-            
-            if !multiSelection.isEmpty && isSelectionMode {
-                VStack(spacing: 0) {
-                    Rectangle()
-                        .foregroundColor(Color("toolbardivider"))
-                        .frame(height: 1)
-                    
-                    HStack {
-                        // TODO: 단어장 이동 버튼; sheet가 올라오고 단어장 목록이 나옴
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "folder")
+        NavigationStack {
+          VStack(spacing: 0) {
+
+              if viewModel.words.isEmpty {
+                  VStack(spacing: 10) {
+                      EmptyWordListView(lang: viewModel.nationality)
+                  }
+                  .foregroundColor(.gray)
+                  .verticalAlignSetting(.center)
+              } else {
+                  FRWordsTableView(viewModel: viewModel, selectedSegment: selectedSegment, unmaskedWords: $unmaskedWords, isSelectionMode: $isSelectionMode, multiSelection: $multiSelection)
+                      .padding(.top, 15)
+              }
+
+              if !multiSelection.isEmpty && isSelectionMode {
+                  VStack(spacing: 0) {
+                      Rectangle()
+                          .foregroundColor(Color("toolbardivider"))
+                          .frame(height: 1)
+
+                      HStack {
+                          // TODO: 단어장 이동 버튼; sheet가 올라오고 단어장 목록이 나옴
+                          Button {
+
+                          } label: {
+                              Image(systemName: "folder")
+                          }
+                          .padding()
+
+                          Spacer()
+
+                          Button("선택한 단어 듣기") {
+                              SpeechSynthesizer.shared.speakWordsAndMeanings(selectedWords, to: "fr-FR")
+                          }
+
+                          Spacer()
+
+                          // TODO: 삭제하기 전에 OO개의 단어를 삭제할거냐고 확인하기 confirmationDialog...
+                          Button(role: .destructive) {
+                              if UIDevice.current.model == "iPhone" {
+                                  confirmationDialog.toggle()
+                              } else if UIDevice.current.model == "iPad" {
+                                  removeAlert.toggle()
+                              }
+                          } label: {
+                              Image(systemName: "trash")
+                          }
+                          .padding()
+                      }
+                      .background(Color("toolbarbackground"))
+                  }
+              }
+          }
+          .navigationDestination(isPresented: $isImportVoca, destination: {
+            ImportCSVFileView(vocabulary: viewModel.selectedVocabulary)
+          })
+          .navigationDestination(isPresented: $isCheckResult, destination: {
+            MyNoteView(words: viewModel.words)
+          })
+          .navigationTitle(isSelectionMode ? "선택된 단어 \(multiSelection.count)개" : navigationTitle)
+          .navigationBarTitleDisplayMode(.inline)
+          .onAppear {
+              viewModel.getVocabulary(vocabularyID: vocabularyID)
+              navigationTitle = viewModel.selectedVocabulary.name ?? ""
+              emptyMessage = viewModel.getEmptyWord()
+          }
+          // 시험 모드 시트
+          .fullScreenCover(isPresented: $isTestMode, content: {
+              TestModeSelectView(isTestMode: $isTestMode, vocabularyID: vocabularyID)
+          })
+          // 단어 여러 개 삭제 여부 (iPhone)
+          .confirmationDialog("단어 삭제", isPresented: $confirmationDialog, actions: {
+              Button(role: .destructive) {
+                  for word in multiSelection {
+                      viewModel.deleteWord(word: word)
+                  }
+                  multiSelection.removeAll()
+                  confirmationDialog.toggle()
+                  isSelectionMode.toggle()
+              } label: {
+                  HStack {
+                      Image(systemName: "trash")
+                      Text("\(multiSelection.count)개의 단어 삭제")
+                  }
+              }
+          })
+          // 단어 여러 개 삭제 여부 (iPad)
+          .alert("\(multiSelection.count)개의 단어를 삭제하시겠습니까?", isPresented: $removeAlert, actions: {
+              Button(role: .cancel) {
+                  removeAlert.toggle()
+              } label: {
+                  Text("Cancle")
+              }
+
+              Button(role: .destructive) {
+                  for word in multiSelection {
+                      viewModel.deleteWord(word: word)
+                  }
+                  multiSelection.removeAll()
+                  removeAlert.toggle()
+                  isSelectionMode.toggle()
+              } label: {
+                  Text("OK")
+              }
+
+          })
+          // 새 단어 추가 시트
+          .sheet(isPresented: $addNewWord) {
+              FRAddNewWordView(viewModel: viewModel)
+                  .presentationDetents([.height(CGFloat(500))])
+          }
+          // 단어장 내보내기
+          .fileExporter(isPresented: $isExport, document: CSVFile(initialText: viewModel.buildDataForCSV() ?? ""), contentType: .commaSeparatedText, defaultFilename: "\(navigationTitle)") { result in
+              switch result {
+              case .success(let url):
+                  print("Saved to \(url)")
+              case .failure(let error):
+                  print(error.localizedDescription)
+              }
+          }
+          .toolbar {
+              // TODO: 편집모드에 따른 toolbar State 분기
+              if !isSelectionMode, isSpeech { // 전체 발음 듣기 모드
+                  ToolbarItem {
+                      Button("취소", role: .cancel) {
+                          isSpeech.toggle()
+                          SpeechSynthesizer.shared.stopSpeaking()
+                      }
+                  }
+              } else if isSelectionMode, !isSpeech {  // 편집 모드
+                  ToolbarItem {
+                      Button("취소", role: .cancel) {
+                          isSelectionMode.toggle()
+                          multiSelection.removeAll()
+                          SpeechSynthesizer.shared.stopSpeaking()
+                      }
+                  }
+              } else {
+  //                ToolbarItem {
+  //                    VStack(alignment: .center) {
+  //                        Text("\(viewModel.words.count)")
+  //                            .foregroundColor(.gray)
+  //                    }
+  //                }
+                  // + 버튼
+                  ToolbarItem {
+                      Button {
+                          addNewWord.toggle()
+                      } label: {
+                          Image(systemName: "plus")
+                      }
+                  }
+
+
+                // 햄버거 버튼
+                ToolbarItem {
+                    CustomMenu(currentMode: $selectedSegment, orderMode: $selectedOrder, speakOn: $isSpeech, testOn: $isTestMode, editOn: $isSelectionMode, isImportVoca: $isImportVoca, isExportVoca: $isExport, isCheckResult: $isCheckResult)
+                        .onChange(of: selectedSegment) { _ in
+                            unmaskedWords = []
                         }
-                        .padding()
-                        
-                        Spacer()
-                        
-                        Button("선택한 단어 듣기") {
-                            SpeechSynthesizer.shared.speakWordsAndMeanings(selectedWords, to: "fr-FR")
-                        }
-                        
-                        Spacer()
-                        
-                        // TODO: 삭제하기 전에 OO개의 단어를 삭제할거냐고 확인하기 confirmationDialog...
-                        Button(role: .destructive) {
-                            if UIDevice.current.model == "iPhone" {
-                                confirmationDialog.toggle()
-                            } else if UIDevice.current.model == "iPad" {
-                                removeAlert.toggle()
+                        .onChange(of: selectedOrder) { value in
+                            switch value {
+                            case "랜덤 정렬":
+                            viewModel.words.shuffle()
+                            case "사전순 정렬":
+                            viewModel.words.shuffle()
+                            default:
+                            viewModel.words.shuffle()
                             }
-                        } label: {
-                            Image(systemName: "trash")
                         }
-                        .padding()
-                    }
-                    .background(Color("toolbarbackground"))
-                }
-            }
-        }
-        .navigationTitle(isSelectionMode ? "선택된 단어 \(multiSelection.count)개" : navigationTitle)
-        .navigationBarTitleDisplayMode(.inline)
-        .onAppear {
-            viewModel.getVocabulary(vocabularyID: vocabularyID)
-            navigationTitle = viewModel.selectedVocabulary.name ?? ""
-            emptyMessage = viewModel.getEmptyWord()
-        }
-        // 시험 모드 시트
-        .fullScreenCover(isPresented: $isTestMode, content: {
-            TestModeSelectView(isTestMode: $isTestMode, vocabularyID: vocabularyID)
-        })
-        // 단어 여러 개 삭제 여부 (iPhone)
-        .confirmationDialog("단어 삭제", isPresented: $confirmationDialog, actions: {
-            Button(role: .destructive) {
-                for word in multiSelection {
-                    viewModel.deleteWord(word: word)
-                }
-                multiSelection.removeAll()
-                confirmationDialog.toggle()
-                isSelectionMode.toggle()
-            } label: {
-                HStack {
-                    Image(systemName: "trash")
-                    Text("\(multiSelection.count)개의 단어 삭제")
-                }
-            }
-        })
-        // 단어 여러 개 삭제 여부 (iPad)
-        .alert("\(multiSelection.count)개의 단어를 삭제하시겠습니까?", isPresented: $removeAlert, actions: {
-            Button(role: .cancel) {
-                removeAlert.toggle()
-            } label: {
-                Text("Cancle")
-            }
-
-            Button(role: .destructive) {
-                for word in multiSelection {
-                    viewModel.deleteWord(word: word)
-                }
-                multiSelection.removeAll()
-                removeAlert.toggle()
-                isSelectionMode.toggle()
-            } label: {
-                Text("OK")
-            }
-
-        })
-        // 새 단어 추가 시트
-        .sheet(isPresented: $addNewWord) {
-            FRAddNewWordView(viewModel: viewModel)
-                .presentationDetents([.height(CGFloat(500))])
-        }
-        // 단어장 내보내기
-        .fileExporter(isPresented: $isExport, document: CSVFile(initialText: viewModel.buildDataForCSV() ?? ""), contentType: .commaSeparatedText, defaultFilename: "\(navigationTitle)") { result in
-            switch result {
-            case .success(let url):
-                print("Saved to \(url)")
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-        .toolbar {
-            // TODO: 편집모드에 따른 toolbar State 분기
-            if !isSelectionMode, isSpeech { // 전체 발음 듣기 모드
-                ToolbarItem {
-                    Button("취소", role: .cancel) {
-                        isSpeech.toggle()
-                        SpeechSynthesizer.shared.stopSpeaking()
-                    }
-                }
-            } else if isSelectionMode, !isSpeech {  // 편집 모드
-                ToolbarItem {
-                    Button("취소", role: .cancel) {
-                        isSelectionMode.toggle()
-                        multiSelection.removeAll()
-                        SpeechSynthesizer.shared.stopSpeaking()
-                    }
-                }
-            } else {
-//                ToolbarItem {
-//                    VStack(alignment: .center) {
-//                        Text("\(viewModel.words.count)")
-//                            .foregroundColor(.gray)
-//                    }
-//                }
-                // + 버튼
-                ToolbarItem {
-                    Button {
-                        addNewWord.toggle()
-                    } label: {
-                        Image(systemName: "plus")
-                    }
-                }
-
-
-              // 햄버거 버튼
-              ToolbarItem {
-                  CustomMenu(currentMode: $selectedSegment, orderMode: $selectedOrder, speakOn: $isSpeech, testOn: $isTestMode, editOn: $isSelectionMode, isImportVoca: $isImportVoca, isExportVoca: $isExport, isCheckResult: $isCheckResult)
-                      .onChange(of: selectedSegment) { _ in
-                          unmaskedWords = []
-                      }
-                      .onChange(of: selectedOrder) { value in
-                          switch value {
-                          case "랜덤 정렬":
-                          viewModel.words.shuffle()
-                          case "사전순 정렬":
-                          viewModel.words.shuffle()
-                          default:
-                          viewModel.words.shuffle()
-                          }
-                      }
-                      .onChange(of: isSpeech) { value in
-                          if value {
-                            SpeechSynthesizer.shared.speakWordsAndMeanings(viewModel.words, to: "en-US")
-                          }
-                      }
-                }
-            }
-        }
-        .onDisappear {
-            SpeechSynthesizer.shared.stopSpeaking()
+                        .onChange(of: isSpeech) { value in
+                            if value {
+                              SpeechSynthesizer.shared.speakWordsAndMeanings(viewModel.words, to: "en-US")
+                            }
+                        }
+                  }
+              }
+          }
+          .onDisappear {
+              SpeechSynthesizer.shared.stopSpeaking()
+          }
         }
     }
 }
