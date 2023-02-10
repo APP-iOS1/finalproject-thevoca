7927116c7f3c0151ce9519eebec5e0bd6352af1e
eigen98 <terrantrace@gmail.com>
2023-02-10 11:26:38 +0900

    Feat(Firebase) : Analystics & Crashlystics를 적용 1. Firebase 앱 등록 및 SPM Crashlytics & Crashlytics추가. 2. Firebase 초기화 3. DSYM 추가. close : #182

--- GGomVoca/GGomVoca/GGomVocaApp.swift
+++ GGomVoca/GGomVoca/GGomVocaApp.swift
@@ -6,10 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct GGomVocaApp: App {
     let persistenceController = PersistenceController.shared
-
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             // environment -> view에 접근
