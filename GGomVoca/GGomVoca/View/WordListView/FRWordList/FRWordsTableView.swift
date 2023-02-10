6b19e653c74ea661598966ff0a3d19f08b3ff3ee
ehvkddl <ehvkddl@naver.com>
2023-02-10 20:30:24 +0900

    design(WordsTableView): Header와 List 사이 공백 제거 #191

--- GGomVoca/GGomVoca/View/WordListView/FRWordList/FRWordsTableView.swift
+++ GGomVoca/GGomVoca/View/WordListView/FRWordList/FRWordsTableView.swift
@@ -23,118 +23,119 @@ struct FRWordsTableView: View {
     @Binding var multiSelection: Set<Word>
     
     var body: some View {
-        // MARK: - Header
-        HStack {
-            if isSelectionMode {
-                Image(systemName: "circle")
-                    .foregroundColor(.clear)
-                    .font(.title2)
-                    .padding(.vertical, -10)
-            }
-            Text("단어")
-                .headerText()
-            Text("뜻")
-                .headerText()
-        }
-        .animation(.default, value: isSelectionMode)
-        .padding(.horizontal, 20)
-        .background {
-            Rectangle()
-                .fill(Color("fourseason"))
-                .frame(height: 30)
-        }
-        
-        // MARK: - Content
-        List($viewModel.words, id: \.self, selection: $multiSelection) { $word in
+        VStack(spacing: 0) {
+            // MARK: - Header
             HStack {
-                // 단어
-                Text(word.word ?? "")
-                    .horizontalAlignSetting(.center)
-                    .multilineTextAlignment(.center)
-                    .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
-                    .animation(.none, value: isSelectionMode)
-                VLine()
-                // 옵션, 뜻
-                HStack(spacing: 0) {
-                    // MARK: 단어의 성별에 따라 표시하는 이미지 변경
-                    switch word.option {
-                    case "f":
-                        Image(systemName: "f.square")
-                            .font(.subheadline)
-                            .opacity(0.5)
-                            .padding(.trailing, 5)
-                    case "m":
-                        Image(systemName: "m.square")
-                            .font(.subheadline)
-                            .opacity(0.5)
-                            .padding(.trailing, 5)
-                    default:
-                        EmptyView()
-                    }
-                    
-                    Text(word.meaning!.joined(separator: ", "))
+                if isSelectionMode {
+                    Image(systemName: "circle")
+                        .foregroundColor(.clear)
+                        .font(.title2)
+                        .padding(.vertical, -10)
                 }
-                .horizontalAlignSetting(.center)
-                .opacity((selectedSegment == .meaningTest && !unmaskedWords.contains(word.id!)) ? 0 : 1)
-                .animation(.none, value: isSelectionMode)
+                Text("단어")
+                    .headerText()
+                Text("뜻")
+                    .headerText()
             }
-            .frame(minHeight: 40)
-            .alignmentGuide(.listRowSeparatorLeading) { d in
-                d[.leading]
+            .animation(.default, value: isSelectionMode)
+            .padding(.horizontal, 20)
+            .background {
+                Rectangle()
+                    .fill(Color("fourseason"))
+                    .frame(height: 30)
             }
-            .overlay {
-                if !isSelectionMode {
-                    Color.clear
-                        .frame(width: UIScreen.main.bounds.width)
-                        .contentShape(Rectangle())
-                        .onTapGesture {
-                            if selectedSegment != .normal {
-                                /// - 편집모드가 아닐때는 단어를 가렸다가 보였다가 할 수 있도록
-                                if unmaskedWords.contains(word.id) {
-                                    if let tmpIndex = unmaskedWords.firstIndex(of: word.id) {
-                                        unmaskedWords.remove(at: tmpIndex)
+            
+            // MARK: - Content
+            List($viewModel.words, id: \.self, selection: $multiSelection) { $word in
+                HStack {
+                    // 단어
+                    Text(word.word ?? "")
+                        .horizontalAlignSetting(.center)
+                        .multilineTextAlignment(.center)
+                        .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+                        .animation(.none, value: isSelectionMode)
+                    VLine()
+                    // 옵션, 뜻
+                    HStack(spacing: 0) {
+                        // MARK: 단어의 성별에 따라 표시하는 이미지 변경
+                        switch word.option {
+                        case "f":
+                            Image(systemName: "f.square")
+                                .font(.subheadline)
+                                .opacity(0.5)
+                                .padding(.trailing, 5)
+                        case "m":
+                            Image(systemName: "m.square")
+                                .font(.subheadline)
+                                .opacity(0.5)
+                                .padding(.trailing, 5)
+                        default:
+                            EmptyView()
+                        }
+                        
+                        Text(word.meaning!.joined(separator: ", "))
+                    }
+                    .horizontalAlignSetting(.center)
+                    .opacity((selectedSegment == .meaningTest && !unmaskedWords.contains(word.id!)) ? 0 : 1)
+                    .animation(.none, value: isSelectionMode)
+                }
+                .frame(minHeight: 40)
+                .alignmentGuide(.listRowSeparatorLeading) { d in
+                    d[.leading]
+                }
+                .overlay {
+                    if !isSelectionMode {
+                        Color.clear
+                            .frame(width: UIScreen.main.bounds.width)
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                if selectedSegment != .normal {
+                                    /// - 편집모드가 아닐때는 단어를 가렸다가 보였다가 할 수 있도록
+                                    if unmaskedWords.contains(word.id) {
+                                        if let tmpIndex = unmaskedWords.firstIndex(of: word.id) {
+                                            unmaskedWords.remove(at: tmpIndex)
+                                        }
+                                    } else {
+                                        unmaskedWords.append(word.id)
                                     }
-                                } else {
-                                    unmaskedWords.append(word.id)
                                 }
                             }
+                    }
+                }
+                .swipeActions(allowsFullSwipe: false) {
+                    Button(role: .destructive){
+                        viewModel.deleteWord(word: word)
+                    }label: {
+                        Label("Delete", systemImage: "trash.fill")
+                    }
+                }
+                .contextMenu {
+                    if selectedSegment == .normal {
+                        Button {
+                            editingWord = word
+                            editWord.toggle()
+                        } label: {
+                            Label("수정하기", systemImage: "gearshape.fill")
                         }
-                }
-            }
-            .swipeActions(allowsFullSwipe: false) {
-                Button(role: .destructive){
-                    viewModel.deleteWord(word: word)
-                }label: {
-                    Label("Delete", systemImage: "trash.fill")
-                }
-            }
-            .contextMenu {
-                if selectedSegment == .normal {
-                    Button {
-                        editingWord = word
-                        editWord.toggle()
-                    } label: {
-                        Label("수정하기", systemImage: "gearshape.fill")
-                    }
-                    Button {
-                        SpeechSynthesizer.shared.speakWordAndMeaning(word, to: "fr-FR", .single)
-                    } label: {
-                        Label("발음 듣기", systemImage: "mic.fill")
+                        Button {
+                            SpeechSynthesizer.shared.speakWordAndMeaning(word, to: "fr-FR", .single)
+                        } label: {
+                            Label("발음 듣기", systemImage: "mic.fill")
+                        }
                     }
                 }
+            } // List
+            .listStyle(.plain)
+            .environment(\.editMode, .constant(self.isSelectionMode ? EditMode.active : EditMode.inactive))
+            .animation(.default, value: isSelectionMode)
+            // MARK: 단어 편집 시트
+            .sheet(isPresented: $editWord) {
+                FREditWordView(viewModel: viewModel, editWord: $editWord, editingWord: $editingWord)
+                    .presentationDetents([.medium])
             }
-        } // List
-        .listStyle(.plain)
-        .padding(.top, -10)
-        .environment(\.editMode, .constant(self.isSelectionMode ? EditMode.active : EditMode.inactive))
-        .animation(.default, value: isSelectionMode)
-        // MARK: 단어 편집 시트
-        .sheet(isPresented: $editWord) {
-            FREditWordView(viewModel: viewModel, editWord: $editWord, editingWord: $editingWord)
-                .presentationDetents([.medium])
-        }
-        .onDisappear {
-            SpeechSynthesizer.shared.stopSpeaking()
+            .onDisappear {
+                SpeechSynthesizer.shared.stopSpeaking()
+            }
         }
     }
 }
--- GGomVoca/GGomVoca/View/WordListView/KOWordList/KOWordsTableView.swift
+++ GGomVoca/GGomVoca/View/WordListView/KOWordList/KOWordsTableView.swift
@@ -23,108 +23,109 @@ struct KOWordsTableView: View {
     @Binding var multiSelection: Set<Word>
     
     var body: some View {
-        // MARK: - Header
-        HStack {
-            if isSelectionMode {
-                Image(systemName: "circle")
-                    .foregroundColor(.clear)
-                    .font(.title2)
-                    .padding(.vertical, -10)
-            }
-            Text("단어")
-                .headerText()
-            Text("발음")
-                .headerText()
-            Text("뜻")
-                .headerText()
-        }
-        .animation(.default, value: isSelectionMode)
-        .padding(.horizontal, 20)
-        .background {
-            Rectangle()
-                .fill(Color("fourseason"))
-                .frame(height: 30)
-        }
-        
-        // MARK: - Content
-        List($viewModel.words, id: \.self, selection: $multiSelection) { $word in
+        VStack(spacing: 0) {
+            // MARK: - Header
             HStack {
-                // 단어
-                Text(word.word ?? "")
-                    .horizontalAlignSetting(.center)
-                    .multilineTextAlignment(.center)
-                    .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
-                    .animation(.none, value: isSelectionMode)
-                VLine()
-                // 발음
-                Text(word.option ?? "")
-                    .horizontalAlignSetting(.center)
-                    .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
-                    .animation(.none, value: isSelectionMode)
-                VLine()
-                // 뜻
-                Text(word.meaning!.joined(separator: ", "))
-                    .horizontalAlignSetting(.center)
-                    .opacity((selectedSegment == .meaningTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
-                    .animation(.none, value: isSelectionMode)
+                if isSelectionMode {
+                    Image(systemName: "circle")
+                        .foregroundColor(.clear)
+                        .font(.title2)
+                        .padding(.vertical, -10)
+                }
+                Text("단어")
+                    .headerText()
+                Text("발음")
+                    .headerText()
+                Text("뜻")
+                    .headerText()
             }
-            .frame(minHeight: 40)
-            .alignmentGuide(.listRowSeparatorLeading) { d in
-                d[.leading]
+            .animation(.default, value: isSelectionMode)
+            .padding(.horizontal, 20)
+            .background {
+                Rectangle()
+                    .fill(Color("fourseason"))
+                    .frame(height: 30)
             }
-            .overlay {
-                if !isSelectionMode {
-                    Color.clear
-                        .frame(width: UIScreen.main.bounds.width)
-                        .contentShape(Rectangle())
-                        .onTapGesture {
-                            if selectedSegment != .normal {
-                                /// - 편집모드가 아닐때는 단어를 가렸다가 보였다가 할 수 있도록
-                                if unmaskedWords.contains(word.id) {
-                                    if let tmpIndex = unmaskedWords.firstIndex(of: word.id) {
-                                        unmaskedWords.remove(at: tmpIndex)
+            
+            // MARK: - Content
+            List($viewModel.words, id: \.self, selection: $multiSelection) { $word in
+                HStack {
+                    // 단어
+                    Text(word.word ?? "")
+                        .horizontalAlignSetting(.center)
+                        .multilineTextAlignment(.center)
+                        .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+                        .animation(.none, value: isSelectionMode)
+                    VLine()
+                    // 발음
+                    Text(word.option ?? "")
+                        .horizontalAlignSetting(.center)
+                        .opacity((selectedSegment == .wordTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+                        .animation(.none, value: isSelectionMode)
+                    VLine()
+                    // 뜻
+                    Text(word.meaning!.joined(separator: ", "))
+                        .horizontalAlignSetting(.center)
+                        .opacity((selectedSegment == .meaningTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
+                        .animation(.none, value: isSelectionMode)
+                }
+                .frame(minHeight: 40)
+                .alignmentGuide(.listRowSeparatorLeading) { d in
+                    d[.leading]
+                }
+                .overlay {
+                    if !isSelectionMode {
+                        Color.clear
+                            .frame(width: UIScreen.main.bounds.width)
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                if selectedSegment != .normal {
+                                    /// - 편집모드가 아닐때는 단어를 가렸다가 보였다가 할 수 있도록
+                                    if unmaskedWords.contains(word.id) {
+                                        if let tmpIndex = unmaskedWords.firstIndex(of: word.id) {
+                                            unmaskedWords.remove(at: tmpIndex)
+                                        }
+                                    } else {
+                                        unmaskedWords.append(word.id)
                                     }
-                                } else {
-                                    unmaskedWords.append(word.id)
                                 }
                             }
+                    }
+                }
+                .swipeActions(allowsFullSwipe: false) {
+                    Button(role: .destructive){
+                        viewModel.deleteWord(word: word)
+                    }label: {
+                        Label("Delete", systemImage: "trash.fill")
+                    }
+                }
+                .contextMenu {
+                    if selectedSegment == .normal {
+                        Button {
+                            editingWord = word
+                            editWord.toggle()
+                        } label: {
+                            Label("수정하기", systemImage: "gearshape.fill")
                         }
-                }
-            }
-            .swipeActions(allowsFullSwipe: false) {
-                Button(role: .destructive){
-                    viewModel.deleteWord(word: word)
-                }label: {
-                    Label("Delete", systemImage: "trash.fill")
-                }
-            }
-            .contextMenu {
-                if selectedSegment == .normal {
-                    Button {
-                        editingWord = word
-                        editWord.toggle()
-                    } label: {
-                        Label("수정하기", systemImage: "gearshape.fill")
-                    }
-                    Button {
-                        SpeechSynthesizer.shared.speakWordAndMeaning(word, to: "ko-KR", .single)
-                    } label: {
-                        Label("발음 듣기", systemImage: "mic.fill")
+                        Button {
+                            SpeechSynthesizer.shared.speakWordAndMeaning(word, to: "ko-KR", .single)
+                        } label: {
+                            Label("발음 듣기", systemImage: "mic.fill")
+                        }
                     }
                 }
+            } // List
+            .listStyle(.plain)
+            .environment(\.editMode, .constant(self.isSelectionMode ? EditMode.active : EditMode.inactive))
+            .animation(.default, value: isSelectionMode)
+            // MARK: 단어 편집 시트
+            .sheet(isPresented: $editWord) {
+                KOEditWordView(viewModel: viewModel, editWord: $editWord, editingWord: $editingWord)
+                    .presentationDetents([.medium])
             }
-        } // List
-        .listStyle(.plain)
-        .padding(.top, -10)
-        .environment(\.editMode, .constant(self.isSelectionMode ? EditMode.active : EditMode.inactive))
-        .animation(.default, value: isSelectionMode)
-        // MARK: 단어 편집 시트
-        .sheet(isPresented: $editWord) {
-            KOEditWordView(viewModel: viewModel, editWord: $editWord, editingWord: $editingWord)
-                .presentationDetents([.medium])
-        }
-        .onDisappear {
-            SpeechSynthesizer.shared.stopSpeaking()
+            .onDisappear {
+                SpeechSynthesizer.shared.stopSpeaking()
+            }
         }
     }
 }
