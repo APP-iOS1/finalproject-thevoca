b47dfcd5b4878c6cc4674ee12b42905cc82b491b
steady-on <whitebsm93@naver.com>
2023-02-12 02:32:29 +0900

    Feat(Refreshable) #196
    - 원활한 데이터 동기화를 위해 아래로 잡아당겨 새로고침 할 수 있도록 refreshable 추가
    - 단어장이 없는 상태에서도 refrashable메서드를 사용할 수 있게 ScrollView 적용
    - 아래로 잡아당겨 데이터를 동기화할 수 있다는 문구 추가

--- GGomVoca/GGomVoca/View/DisplaySplitView/DisplaySplitView.swift
+++ GGomVoca/GGomVoca/View/DisplaySplitView/DisplaySplitView.swift
@@ -66,6 +66,9 @@ struct DisplaySplitView: View {
         .onAppear {
             viewModel.getVocabularyData()
         }
+        .refreshable {
+            viewModel.getVocabularyData()
+        }
     }
     
     // MARK: VocabularyList의 상태에 따라 분기되는 Sidebar View 및 공통 수정자
@@ -108,13 +111,20 @@ struct DisplaySplitView: View {
     
     // MARK: VocabularyList가 비어있을 때 표시되는 sidebar View
     func emptyVocabularyListView() -> some View {
-        VStack(spacing: 10) {
-            Text("단어장 없음").font(.title3)
-            Text("하단의 \(Image(systemName: "folder.badge.plus"))을 눌러 단어장을 생성하세요")
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .center, spacing: 10) {
+                    Text("단어장 없음").font(.title3)
+                    Text("하단의 \(Image(systemName: "folder.badge.plus"))을 눌러 단어장을 생성하세요.")
+                    Text("(혹은 아래로 잡아당겨 데이터 동기화)")
+                }
+                .foregroundColor(.gray)
+                .padding()
+                .padding(.bottom, 40)
+                .horizontalAlignSetting(.center)
+                .frame(minHeight: geometry.size.height)
+            }
         }
-        .foregroundColor(.gray)
-        .padding()
-        .padding(.bottom, 40)
     }
 
     // MARK: VocabularyList가 비어있지 않을 때 표시되는 sidebar view
