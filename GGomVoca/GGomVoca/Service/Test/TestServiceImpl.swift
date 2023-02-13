1c4205749c8b3a71bcefb299cce936eed04cd774
eigen98 <terrantrace@gmail.com>
2023-02-13 15:03:42 +0900

    Feat(TestService) : viewModel이 완성되기 전까지 구현(완성 된 후 마저 작업) close : #200

--- GGomVoca/GGomVoca/Service/Test/TestServiceImpl.swift
+++ GGomVoca/GGomVoca/Service/Test/TestServiceImpl.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 class TestServiceImpl : TestService{
     
     //Repository
@@ -22,5 +23,19 @@ class TestServiceImpl : TestService{
         coreDataRepo.saveContext()
     }
     
+    // MARK: 일치하는 id의 단어장 불러오기
+    func getVocabularyFromId(vocabularyID: Vocabulary.ID) -> AnyPublisher<Vocabulary, RepositoryError> {
+        return coreDataRepo.getVocabularyFromID(vocabularyID: vocabularyID!)
+    }
     
+    // MARK: 일치하는 id의 단어장 단어리스트 불러오기
+    //TODO: Cloud Fetch
+    func fetchWordList(vocabulary: Vocabulary) -> AnyPublisher<[Word], RepositoryError> {
+        return coreDataRepo.getWordListFromVoca(voca: vocabulary)
+    }
+    
+    // MARK: - Core Data에 시험결과 저장
+    func testResult() {
+       
+    }
 }
