11c2cece1b289d286d5690085d163a868eb82d81
steady-on <whitebsm93@naver.com>
2023-02-14 14:30:38 +0900

    Feat(WordsTableView) #208
    - 디바이스 별로 텍스트 크기를 다르게 하기 위해서 수정자 적용

--- GGomVoca/GGomVoca/View/WordListView/JPWordList/JPWordsTableView.swift
+++ GGomVoca/GGomVoca/View/WordListView/JPWordList/JPWordsTableView.swift
@@ -65,6 +65,7 @@ struct JPWordsTableView: View {
                         .listCellText(isSelectionMode: isSelectionMode)
                         .opacity((selectedSegment == .meaningTest && !unmaskedWords.contains(word.id)) ? 0 : 1)
                 }
+                .eachDeviceFontSize()
                 .frame(minHeight: 40)
                 .alignmentGuide(.listRowSeparatorLeading) { d in
                     d[.leading]
