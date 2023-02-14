882959c9272dbf5af45544ae39f2d1d9d2cd9edf
gxdxt <yxxsxxny@gmail.com>
2023-02-14 15:39:58 +0900

    feat(String+): Casting String to NSLocalizedString added #195

--- GGomVoca/GGomVoca/Extension/String+.swift
+++ GGomVoca/GGomVoca/Extension/String+.swift
@@ -8,13 +8,18 @@
 import Foundation
 
 extension String {
-  // MARK: 여러 개의 뜻을 가졌는지 확인
-  var multiCheck: Bool {
-    self.contains(",") ? true : false
-  }
+    // MARK: 여러 개의 뜻을 가졌는지 확인
+    var multiCheck: Bool {
+        self.contains(",") ? true : false
+    }
 
-  // MARK: CSV에서 여러 개의 뜻을 하나로 인식하도록 재구성
-  var reformForCSV: String {
-    "\"\(self)\""
-  }
+    // MARK: CSV에서 여러 개의 뜻을 하나로 인식하도록 재구성
+    var reformForCSV: String {
+        "\"\(self)\""
+    }
+
+    // MARK: UIKit 부분 localization
+    var localized: String {
+        return NSLocalizedString(self, tableName: "Localizable", value: self, comment: "")
+     }
 }
