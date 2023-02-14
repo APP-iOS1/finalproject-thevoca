49d95dd50edd3cabac62dbc595051505a34c9fa0
steady-on <whitebsm93@naver.com>
2023-02-14 14:04:18 +0900

    Feat(FontSize) #208
    - device 별로 font 사이즈를 다르게 설정하는 메서드 구현

--- GGomVoca/GGomVoca/Extension/View+.swift
+++ GGomVoca/GGomVoca/Extension/View+.swift
@@ -35,6 +35,15 @@ extension View {
             .animation(.none, value: isSelectionMode)
     }
     
+    // MARK: device별로 font size를 다르게 적용
+    func eachDeviceFontSize() -> some View {
+        if UIDevice.current.model == "iPad" {
+            return self.font(.title3)
+        } else {
+            return self.font(.body)
+        }
+    }
+    
     // MARK: Custom Swipe
     func addSwipeButtonActions(leadingButtons: [CellButtons], trailingButton: [CellButtons], onClick: @escaping (CellButtons) -> Void) -> some View {
         self.modifier(SwipeContainerCell(leadingButtons: leadingButtons, trailingButton: trailingButton, onClick: onClick))
